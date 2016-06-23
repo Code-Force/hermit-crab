28db9af58275a9023d8c40ff1297107b7090878e
Edi Bouazza <edi@bencrudo.com>
2016-06-22 22:03:08 -0400

    no message

--- App/application/views/pages/home.php
+++ App/application/views/pages/home.php
@@ -3,11 +3,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
 
 <div id="container">
-	<h1>Welcome to CodeIgniter!</h1>
+	<h1>Welcome to being a towel</h1>
 
 	<div id="body">
-
-	HEY BENNYYYYYY
+		No you're a towel
 	</div>
 
 	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
