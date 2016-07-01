64226c052f1f5e45790acea3f26314187964f3f7
Edi Bouazza <edi@bencrudo.com>
2016-07-01 01:11:12 -0400

    Moved the AJAX login view to a newly-created AJAX views folder.

---
+++ App/application/views/ajax/login_ajax_view.php
@@ -0,0 +1,10 @@
+<?php echo validation_errors(); ?>
+<?php echo form_open('login/verify'); ?>
+	<label for="username">Username:</label>
+	<input type="text" id="username" name="username"/>
+	<br/>
+	<label for="password">Password:</label>
+	<input type="password"  id="passowrd" name="password"/>
+	<br/>
+	<input type="submit" value="Login"/>
+</form>
