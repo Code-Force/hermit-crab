d64c8cbb727cc88b84f882adc57004139bef4ea8
Edi Bouazza <edi@bencrudo.com>
2016-09-28 20:29:17 -0400

    Merge

--- App/application/views/snippets/user_nav_view.php
+++ App/application/views/snippets/user_nav_view.php
@@ -1,13 +1,48 @@
 <div class="container-fluid">
 	<div class="row">
+<<<<<<< HEAD
 		<div class="col-xs-30 col-left">
+=======
+		<div class="col-xs-10 col-left">
+>>>>>>> master
 			<div class="logo-holder">
 				<a href="/">
 					<img class="logo-holder__logo" src="<?= base_url(); ?>assets/images/logo/logo_badge--300.png" />
 				</a>
 			</div>
 		</div>
+<<<<<<< HEAD
 		<div class="col-xs-30 col-right">
+=======
+		<div class="col-xs-40 col-center">
+			<div id="quick-nav">
+				<?php
+				$quick_nav_html = '';
+				?>
+				<div class="filters">
+					<div class="filters__mood-container">
+						<?php
+						if (isset($categories)) {
+							foreach ($categories as $category) {
+								$quick_nav_html .= '
+					<input type="checkbox" class="filter-category cat-icon" id="'.$category['handle'].'" name="filter-categories" value="'.$category['category_id'].'" /><label for="'.$category['handle'].'" title="'.$category['name'].'" style="background: url(\''.base_url().'/assets/images/map/icons/'.$category['handle_img'].'.png\')"></label> ';
+							}
+						}
+						?>
+						<div class="categories">
+							<?php
+							echo $quick_nav_html;
+							?>
+						</div>
+						<!--<a href="#" class="clear-filters" data-clear-type="checkbox" data-clear-identifier="category">Clear Categories</a>-->
+					</div>
+				</div>
+
+				<!--<div id="nav-toggle"></div>-->
+			</div>
+		</div>
+		<div class="col-xs-10 col-right">
+>>>>>>> master
 			<div class="header__account">
 				<?
 				if(isset($user) && $user) { ?>
