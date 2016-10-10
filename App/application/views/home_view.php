1e6ddce8ea5f248dba87d173149aa2fed0d11db3
Edi Bouazza <edi@bencrudo.com>
2016-10-10 17:45:52 -0400

    Added quick action icons on the home page and descriptions

--- App/application/views/home_view.php
+++ App/application/views/home_view.php
@@ -5,8 +5,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<div id="home-grid" class="home__grid">
 		<?php foreach ($stories as $key=>$story) { ?>
 			<div class="home__element" data-date="<?= $story['date_posted']; ?>">
-				<a href="<?= base_url().'stories/'.$story['slug']; ?>">
-					<div class="home-story">
+				<div class="home-story">
+					<a href="<?= base_url().'stories/'.$story['slug']; ?>">
 						<div class="home-story__image-wrapper">
 							<img class="home-story__image" src="<?= base_url().'assets/users/'.$story['folder'].'/stories/'.$story['album'].'/'.$story['story_cover'] ?>" alt="<?= $story['story_title']; ?>">
 						</div>
@@ -16,28 +16,26 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 								<i class="home-story__location-pin fa fa-map-marker" aria-hidden="true"></i>
 								<span class="home-story__location-name"><?= $story['location']; ?></span>
 							</p>
-						</div>
-						<div class="home-story__quick-actions">
-							<div class="home-story__quick-action home-story__quick-action--share">
-								<i class="home-story__action-icon fa fa-share" aria-hidden="true" title="Share"></i>
-							</div>
-							<div class="home-story__quick-action home-story__quick-action--read-later">
-								<i class="home-story__action-icon fa fa-clock-o" aria-hidden="true" title="Read later"></i>
-							</div>
-							<div class="home-story__quick-action home-story__quick-action--description">
-								<i class="home-story__action-icon fa fa-align-left" aria-hidden="true" title="Description"></i>
-							</div>
-							<div class="home-story__quick-action home-story__quick-action--like">
-								<i class="home-story__action-icon fa fa-heart" aria-hidden="true" title="Like"></i>
+							<div class="home-story__description">
+								<p><?= $story['description']; ?></p>
 							</div>
 						</div>
-						<div class="home-story__description">
-							<p><?= $story['description']; ?></p>
+					</a>
+					<div class="home-story__quick-actions">
+						<div class="home-story__quick-action home-story__quick-action--share">
+							<i class="home-story__action-icon fa fa-share" aria-hidden="true" title="Share"></i>
 						</div>
-						<!--<p><?= $story['description']; ?></p>
-						<a href="<?= base_url().'stories/'.$story['slug']; ?>">View Story</a>-->
+						<div class="home-story__quick-action home-story__quick-action--read-later">
+							<i class="home-story__action-icon fa fa-clock-o" aria-hidden="true" title="Read later"></i>
+						</div>
+						<div class="home-story__quick-action home-story__quick-action--description">
+							<i class="home-story__action-icon fa fa-align-left" aria-hidden="true" title="Excerpt"></i>
+						</div>
+						<div class="home-story__quick-action home-story__quick-action--like">
+							<i class="home-story__action-icon fa fa-heart" aria-hidden="true" title="Like"></i>
+						</div>
 					</div>
-				</a>
+				</div>
 			</div>
 		<?php } ?>
 	</div>
--- App/assets/scripts/home-scripts.js
+++ App/assets/scripts/home-scripts.js
@@ -1,3 +1,7 @@
 head.ready(function() {
 	$('#home-grid').masonry();
+
+	$(document).on('click', '.home-story__quick-action--description', function() {
+		$(this).closest('.home-story').toggleClass('excerpt');
+	});
 });
